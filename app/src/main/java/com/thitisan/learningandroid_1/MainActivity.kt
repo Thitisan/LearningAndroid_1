730ca096d9d623d66b536b3402753852fa638e71
Thitisan <thitisan.th@gmail.com>
2020-02-04 14:25:54 +0700

    Initial commit

---
+++ app/src/main/java/com/thitisan/learningandroid_1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.thitisan.learningandroid_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
